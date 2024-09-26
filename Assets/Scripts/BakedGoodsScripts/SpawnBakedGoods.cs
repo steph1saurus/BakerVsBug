ff6768b55b62a40b95afdf248868c05adf02075d
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-09-26 19:11:13 -0400

    updated spawnbakedGoods

--- Assets/Scripts/BakedGoodsScripts/SpawnBakedGoods.cs
+++ Assets/Scripts/BakedGoodsScripts/SpawnBakedGoods.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class SpawnBakedGoods : MonoBehaviour
 {
+    public GameObject[] bakedGoods;
+    private Vector3 spawnPosition;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        spawnPosition = new Vector3(0, 0, 0);
+        Instantiate(bakedGoods[0], spawnPosition, Quaternion.identity);
         
     }
 
