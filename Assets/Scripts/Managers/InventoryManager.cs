93257fee4b5cd5a7b3377732f116e48b318130e7
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-09-26 12:21:46 -0400

    open/close inventory. move pie

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -1,15 +1,23 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
+    [Header ("Inventory items")]
     public GameObject swatterPrefab; //reference to the prefab asset
     private GameObject instantiatedSwatter; //reference to the instantiated swatter
+
+    [Header("Inventory UI")]
+    public GameObject inventoryPanel;
+    private bool inventoryActive= true;
 
 
     [Header("Variables")]
     public bool swatterSelected = false;
 
+
+    
 
     public void OnMouseDown()
     {
@@ -40,5 +48,20 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
+    public void ActivateInventory()
+    {
+        if (!inventoryActive)
+        {
+            inventoryPanel.SetActive(true);
+            inventoryActive = true;
+        }
+        else
+        {
+            inventoryPanel.SetActive(false);
+            inventoryActive = false;
+        }
+    }
+       
 
+   
 }
