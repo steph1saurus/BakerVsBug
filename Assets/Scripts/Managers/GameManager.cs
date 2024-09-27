61cc8867cc31c81e4c5d6c43fd9663fbbbb8bcaa
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-09-27 14:14:10 -0400

    add reward scene

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Game objects")]
+    public Slider progressBar;
+    public GameObject completionText;
+
+    [Header ("Variables")]
+  
+
+
+    [Header("Checks")]
+    private bool levelCompleteBool = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        LevelComplete();
     }
+
+
+    private void LevelComplete()
+    {
+        if (progressBar.value ==1 && !levelCompleteBool)
+        {
+            levelCompleteBool = true;
+            completionText.SetActive(true);
+                
+                Time.timeScale = 0;
+            StartCoroutine(HandleLevelCompletion());
+        }
+    }
+
+    private IEnumerator HandleLevelCompletion()
+    {
+      
+
+        // Wait for 3 seconds
+        yield return new WaitForSecondsRealtime(3f);  // Realtime because Time.timeScale is set to 0
+
+        // Load the reward scene (with build index 1)
+        SceneManager.LoadScene(1);  // Build index 1 is the Reward scene
+    }
+
 }
+
